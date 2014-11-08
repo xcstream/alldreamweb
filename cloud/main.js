b06cd2f7aba47b3a4a072ee89abc6bf43c8544cd
xcstream <xcstream@vip.qq.com>
2014-11-08 23:58:45 +0800

    http.get

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,12 @@
 require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
+
+var http = require('http')
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+});
+
+AV.Cloud.afterSave("Message", function(request) {
+	http.get("http://itms.sinaapp.com/sms/send.php?message=%E6%9C%89%E6%96%B0%E7%9A%84%E7%95%99%E8%A8%80%E4%BA%86",function(){});
 });
